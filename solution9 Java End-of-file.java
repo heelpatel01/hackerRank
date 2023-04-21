880e6c8df4f33702619475dbccd1f86be3f221ec
HEEL PATEL <91087524+heelpatel01@users.noreply.github.com>
2023-04-21 14:14:23 +0530

    Create solution9 Java End-of-file.java

---
+++ solution9 Java End-of-file.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+              Scanner sc = new Scanner(System.in);
+               int val =1;
+               while(sc.hasNext()){
+               String s = sc.nextLine();
+               System.out.println(val+" "+s);
+               val++;
+            }}}
